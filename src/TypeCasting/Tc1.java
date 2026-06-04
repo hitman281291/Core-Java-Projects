78fc677bd3840716816ba56a03a209d6d3ffa643
hitman281291 <hitesh.purohit28@gmail.com>
2026-06-04 23:52:45 +0530

    Widening

---
+++ src/TypeCasting/Tc1.java
@@ -0,0 +1,13 @@
+void main() {
+    byte b = 10;
+    int i = b; //widening
+    
+    IO.println(i);
+    
+    //-----------------------
+
+    short s = 123;
+    long l = s; //widening
+    
+    IO.println(l);
+}
