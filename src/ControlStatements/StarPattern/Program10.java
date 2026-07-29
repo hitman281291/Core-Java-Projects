ff4b7136d54866394b614145e0efe122efbb1de5
hitman281291 <hitesh.purohit28@gmail.com>
2026-07-29 23:52:47 +0530

    Control statements Tasks - star pattern

---
+++ src/ControlStatements/StarPattern/Program10.java
@@ -0,0 +1,37 @@
+void main()
+{
+
+    for (int i = 1; i <=4 ; i++)
+    {
+        for (int j = 4; j >= i; j--)
+        {
+            IO.print(" ");
+        }
+
+        for (int j = 4; j >= i; j--)
+        {
+            IO.print(" ");
+        }
+
+        for (int j = 1; j <=i ; j++)
+        {
+            IO.print("* ");
+        }
+        IO.println();
+    }
+
+    for (int i=1; i<=5; i++)
+    {
+        for (int j = 2; j <=i ; j++) {
+            IO.print("  ");
+        }
+        
+        for(int j=5; j>=i; j--)
+        {
+            IO.print("* ");
+        }
+
+        IO.println(" ");
+    }
+
+}
---
+++ src/ControlStatements/StarPattern/Program9.java
@@ -0,0 +1,17 @@
+void main()
+{
+    for (int i=1; i<=5; i++)
+    {
+        for (int j = 2; j <=i ; j++) {
+            IO.print("  ");
+        }
+        
+        for(int j=5; j>=i; j--)
+        {
+            IO.print("* ");
+        }
+        
+        IO.println(" ");
+    }
+
+}
